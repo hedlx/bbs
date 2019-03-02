7cef628a2f0ec22d1d2058876374cfd186c0b9a7
Albert Safin <xzfcpw@gmail.com>
2019-03-02 13:31:23 +0700

    Deploy experiment [6]

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,13 +1,10 @@
 echo '$ ls backend/target'
-ls target
+ls backend/target
 echo "rc: $?"
 
 echo '$ date > date.txt'
 date > date.txt
 echo "rc: $?"
-
-echo '$ pwd; cd; pwd; ls'
-pwd; cd; pwd; ls
 
 echo '$ openssl'
 openssl aes-256-cbc -K $encrypted_db197bbd43df_key -iv $encrypted_db197bbd43df_iv -in travis/ssh-key.enc -out travis/ssh-key -d
