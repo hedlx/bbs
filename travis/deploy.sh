0fc30cb4c4560222f8db609ed9eea102cfcc43a0
Albert Safin <xzfcpw@gmail.com>
2019-03-02 14:18:26 +0700

    Deploy experiment [11]

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -14,6 +14,14 @@ echo '$ openssl'
 openssl aes-256-cbc -K $encrypted_db197bbd43df_key -iv $encrypted_db197bbd43df_iv -in travis/ssh-key.enc -out travis/ssh-key -d
 echo "rc: $?"
 
-echo '$ $secret_ssh -i travis/ssh-key ls'
-ssh -q hedlx.org -p 359 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i travis/ssh-key ls
+echo '$ ls -l travis/ssh-key'
+ls -l travis/ssh-key
+echo "rc: $?"
+
+echo '$ sha512sum travis/ssh-key'
+sha512sum travis/ssh-key
+echo "rc: $?"
+
+echo '$ ssh'
+ssh hedlx.org -p 359 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i travis/ssh-key ls
 echo "rc: $?"
