7936f4491a7864412c6cfb43654afeb2b363fa97
Albert Safin <xzfcpw@gmail.com>
2019-03-05 11:41:27 +0700

    [back] refactor db, fix returning ts

--- backend/src/data.rs
+++ backend/src/data.rs
@@ -1,4 +1,8 @@
+use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
+use super::schema::{messages, threads};
+
+/* REST-related */
 
 #[derive(Deserialize)]
 pub struct NewMessage {
@@ -22,4 +26,33 @@ pub struct Thread {
     pub id: u32,
     pub op: Message,
     pub last: Vec<Message>,
+}
+
+/* DB-related */
+
+#[derive(QueryableByName, Insertable, Queryable)]
+#[table_name = "messages"]
+pub struct DbMessage {
+    pub thread_id: i32,
+    pub no: i32,
+    pub name: Option<String>,
+    pub trip: Option<String>,
+    pub sender: String,
+    pub text: String,
+    pub ts: NaiveDateTime,
+}
+
+#[derive(QueryableByName)]
+#[table_name = "threads"]
+pub struct DbThread {
+    pub id: i32,
+    pub last_reply_no: i32,
+    pub bump: NaiveDateTime,
+}
+
+#[derive(Insertable)]
+#[table_name = "threads"]
+pub struct DbNewThread {
+    pub last_reply_no: i32,
+    pub bump: NaiveDateTime,
 }
--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,4 +1,4 @@
-use super::data::{NewMessage, Message, Thread};
+use super::data::{NewMessage, Message, Thread, DbMessage, DbThread, DbNewThread};
 use super::schema::{messages, threads};
 use chrono::{NaiveDateTime, Utc};
 
@@ -21,41 +21,13 @@ struct Id {
     id: i32,
 }
 
-#[derive(QueryableByName, Insertable, Queryable)]
-#[table_name = "messages"]
-struct DbMessage {
-    thread_id: i32,
-    no: i32,
-    name: Option<String>,
-    trip: Option<String>,
-    sender: String,
-    text: String,
-    ts: NaiveDateTime,
-}
-
-#[derive(QueryableByName)]
-#[table_name = "threads"]
-struct DbThread {
-    id: i32,
-    last_reply_no: i32,
-    bump: NaiveDateTime,
-}
-
-#[derive(Insertable)]
-#[table_name = "threads"]
-struct DbNewThread {
-    last_reply_no: i32,
-    bump: NaiveDateTime,
-}
-
-
-// TODO: wrap into transactions
-
 impl Db {
-    pub fn new_thread(&self, msg: NewMessage) {
+    /* NB: public methods must be wrapped in transactions. */
+
+    pub fn new_thread(&self, msg: NewMessage) -> Message {
         let now = Utc::now().naive_utc();
 
-        self.0.transaction::<_, diesel::result::Error, _>(|| {
+        self.transaction(|| {
             let thread_id =
             insert_into(super::schema::threads::dsl::threads).values(
                 &DbNewThread{
@@ -65,18 +37,20 @@ impl Db {
                 .returning(super::schema::threads::dsl::id)
                 .get_result(&self.0)?;
 
+            let result = msg_to_db_msg(msg, now, thread_id, 0);
+
             insert_into(super::schema::messages::dsl::messages)
-                .values(&msg_to_db_msg(msg, now, thread_id, 0))
+                .values(&result)
                 .execute(&self.0)?;
 
-            Ok(())
+            Ok(db_msg_to_msg(result))
         }).unwrap()
     }
 
     pub fn reply_thread(&self, thread_id: i32, msg: NewMessage) -> bool {
         let now = Utc::now().naive_utc();
 
-        self.0.transaction::<_, diesel::result::Error, _>(|| {
+        self.transaction(|| {
             let no = sql_query(r"
                 UPDATE threads
                    SET last_reply_no = last_reply_no+1,
@@ -103,27 +77,29 @@ impl Db {
     }
 
     pub fn get_threads_before(&self, ts: u32, limit: u32) -> Vec<Thread> {
-        let threads = sql_query(r"
-            SELECT id, last_reply_no, bump
-              FROM threads
-             WHERE bump > $1
-             ORDER BY (bump, id) ASC
-             LIMIT $2
-        ")
-        .bind::<Timestamp, _>(NaiveDateTime::from_timestamp(ts as i64, 0))
-        .bind::<Integer, _>(limit as i32)
-        .load::<DbThread>(&self.0)
-        .unwrap();
-
-        threads
-            .iter()
+        self.transaction(|| {
+            let threads = sql_query(r"
+                SELECT id, last_reply_no, bump
+                  FROM threads
+                 WHERE bump > $1
+                 ORDER BY (bump, id) ASC
+                 LIMIT $2
+            ")
+            .bind::<Timestamp, _>(NaiveDateTime::from_timestamp(ts as i64, 0))
+            .bind::<Integer, _>(limit as i32)
+            .load::<DbThread>(&self.0)?
+            .into_iter()
             .map(|thread| Thread {
                 id: thread.id as u32,
                 op: self.get_op(thread.id),
                 last: self.get_last(thread.id).unwrap(),
             })
-            .collect()
+            .collect();
+            Ok(threads)
+        }).unwrap()
     }
+
+    /* Private methods. */
 
     fn get_op(&self, thread_id: i32) -> Message {
         let op = sql_query(r"
@@ -135,7 +111,7 @@ impl Db {
         .bind::<Integer, _>(thread_id)
         .get_result::<DbMessage>(&self.0)
         .unwrap();
-        db_msg_to_msg( &op )
+        db_msg_to_msg( op )
     }
 
     fn get_last(&self, thread_id: i32) -> QueryResult<Vec<Message>> {
@@ -145,8 +121,8 @@ impl Db {
             .filter(d::no.gt(0))
             .limit(5)
             .get_results::<DbMessage>(&self.0)?
-            .iter()
-            .map(|msg| { db_msg_to_msg(msg) })
+            .into_iter()
+            .map(db_msg_to_msg)
             .collect();
         Ok(result)
     }
@@ -165,16 +141,8 @@ impl Db {
         .unwrap();
 
         let messages: Vec<Message> = messages
-            .iter()
-            .map(|mut msg| {
-                Message {
-                    no:   msg.no as u32,
-                    name: msg.name.clone().unwrap_or("Anonymous".to_string()),
-                    trip: msg.trip.clone().unwrap_or("".to_string()),
-                    text: msg.text.clone(),
-                    ts: 0,
-                }
-            })
+            .into_iter()
+            .map(db_msg_to_msg)
             .collect();
 
         if messages.is_empty() {
@@ -183,14 +151,20 @@ impl Db {
             Some(messages)
         }
     }
+
+    fn transaction<T, F>(&self, f: F) -> Result<T, diesel::result::Error>
+    where F: FnOnce() -> Result<T, diesel::result::Error>,
+    {
+        self.0.transaction::<_, diesel::result::Error, _>(f)
+    }
 }
 
-fn db_msg_to_msg(msg: &DbMessage) -> Message {
+fn db_msg_to_msg(msg: DbMessage) -> Message {
     Message {
         no: msg.no as u32,
-        name: msg.name.clone().unwrap_or("Anonymous".to_string()),
-        trip: msg.trip.clone().unwrap_or("".to_string()),
-        text: msg.text.clone(),
+        name: msg.name.unwrap_or("Anonymous".to_string()),
+        trip: msg.trip.unwrap_or("".to_string()),
+        text: msg.text,
         ts: msg.ts.timestamp(),
     }
 }
