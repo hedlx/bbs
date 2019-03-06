d919502187729cb1d1e5b0da41f7002f69ac3011
Albert Safin <xzfcpw@gmail.com>
2019-03-06 17:30:58 +0700

    [back] move code to http.rs

---
+++ backend/src/http.rs
@@ -0,0 +1,94 @@
+// TODO: multipart upload https://github.com/SergioBenitez/Rocket/issues/106
+
+use rocket_contrib::json::Json;
+use rocket::http::{Status, ContentType};
+use serde::Serialize;
+
+use data::{NewMessage, Message, Thread};
+use db::Db;
+
+type Error = rocket::response::status::Custom<rocket_contrib::json::JsonValue>;
+fn error(status: Status, error: &'static str) -> Error {
+    rocket::response::status::Custom(status, json!({"error": error}))
+}
+
+#[get("/threads?<before>&<after>&<limit>&<tag>")]
+fn threads_list(
+    db: Db,
+    before: Option<u32>, // timestamp
+    after: Option<u32>,  // timestamp
+    limit: Option<u32>,
+    tag: Option<String>,
+) -> Json<Vec<Thread>> {
+    let limit = limit.unwrap_or(100);
+    let resp = match (before, after) {
+        (None, None) => db.get_threads_before(0, limit),
+        (Some(ts), None) => db.get_threads_before(ts, limit),
+        (None, Some(_)) => Vec::new(),
+        (Some(_), Some(_)) => Vec::new(),
+    };
+    Json(resp)
+}
+
+#[get("/threads/<id>?<before>&<after>&<limit>")]
+fn thread_id(
+    db: Db,
+    id: i32,
+    before: Option<u32>, // message id
+    after: Option<u32>,  // message id
+    limit: Option<u32>,
+) -> Option<Json<Vec<Message>>> {
+    let limit = limit.unwrap_or(100);
+    match (before, after) {
+        (None, None) => {
+            db.get_thread_messages(id).map(Json)
+        }
+        (Some(_), None) => { None }    // before
+        (None, Some(_)) => { None }    // after
+        (Some(_), Some(_)) => { None } // range / 400
+    }
+}
+
+#[post("/threads", format = "json", data = "<msg>")]
+fn thread_new(db: Db, msg: Json<NewMessage>) -> &'static str {
+    db.new_thread(msg.0);
+    "{}"
+}
+
+#[post("/threads/<id>", format = "json", data = "<msg>")]
+fn thread_reply(
+    db: Db,
+    id: i32,
+    msg: Json<NewMessage>,
+) -> Result<&'static str, Error> {
+    if db.reply_thread(id, msg.0) {
+        Ok("{}")
+    } else {
+        Err(error(Status::NotFound, "no such thread"))
+    }
+}
+
+#[delete("/threads/<id>/replies/<no>")]
+fn thread_reply_delete(
+    db: Db,
+    id: i32,
+    no: i32,
+) -> Option<&'static str> {
+    Some("NIY")
+}
+
+pub fn start() {
+    rocket::ignite()
+        .attach(Db::fairing())
+        .mount(
+            "/",
+            routes![
+                thread_id,
+                thread_new,
+                thread_reply,
+                thread_reply_delete,
+                threads_list,
+            ],
+        )
+        .launch();
+}
