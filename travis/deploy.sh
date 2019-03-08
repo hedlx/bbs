b80144a4c762afc728be8eba6c2fe8a1ee928f68
esterkimx <esterkimx@gmail.com>
2019-03-08 23:05:26 +0200

    [front-elm][travis] Remove favicon (it was added into bbs root dir by hands)
    commit 7c4f97889115219727ea6cb5c5459c908b127f9e
    Author: esterkimx <esterkimx@gmail.com>
    Date:   Fri Mar 8 23:05:07 2019 +0200
    
        remove favicon

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -61,7 +61,7 @@ elm)
 	# TODO move to ~/tmp/elm
 	ssh -C $ssh_opts hedlx.org 'rm -rf front-elm.tmp'
 	scp -C $ssh_opts -r front-elm/static hedlx.org:front-elm.tmp
-	ssh -C $ssh_opts hedlx.org 'cd front-elm.tmp && mv index.html main.js favicon.ico /srv/www/bbs/elm/ && echo moved'
+	ssh -C $ssh_opts hedlx.org 'cd front-elm.tmp && mv index.html main.js /srv/www/bbs/elm/ && echo moved'
 	;;
 clojure)
 	[ -f "./front/resources/public/index.html" ] || die "No front/resources/public/index.html"
